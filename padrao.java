76e238f3abcd41f6279ac432190aa0911f0c85b2
ramonrodmor <ramonrodriguesmo@gmail.com>
2016-09-23 14:30:51 -0300

    Add files via upload

---
+++ padrao.java
@@ -0,0 +1,41 @@
+
+public class padrao {
+
+	float componente1;
+	float componente2;
+	float componente3;
+	float componente4;
+	
+	padrao(){
+		componente1 = 0;
+		componente2 = 0;
+		componente3 = 0;
+		componente4 = 0;
+	}
+	
+	public float getComponente1() {
+		return componente1;
+	}
+	public void setComponente1(float componente1) {
+		this.componente1 = componente1;
+	}
+	public float getComponente2() {
+		return componente2;
+	}
+	public void setComponente2(float componente2) {
+		this.componente2 = componente2;
+	}
+	public float getComponente3() {
+		return componente3;
+	}
+	public void setComponente3(float componente3) {
+		this.componente3 = componente3;
+	}
+	public float getComponente4() {
+		return componente4;
+	}
+	public void setComponente4(float componente4) {
+		this.componente4 = componente4;
+	}
+	 
+}

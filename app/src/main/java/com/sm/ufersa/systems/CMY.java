b55b9529c0c9fbaeb7ef99c86c969fedbbb98c29
Dijon Neto <jdoneto@hotmail.com>
2016-10-04 17:44:37 -0300

    Correção do algoritmo CMY.

--- app/src/main/java/com/sm/ufersa/systems/CMY.java
+++ app/src/main/java/com/sm/ufersa/systems/CMY.java
@@ -8,21 +8,29 @@ public class CMY implements SystemBase{
         float g = rgb.getComponent2()/255;
         float b = rgb.getComponent3()/255;
 
-        float k = 1 - Math.max(r, Math.max(g, b));
+//        float k = 1 - Math.max(r, Math.max(g, b));
 
         float c, m, y;
 
-        c = (1 - r - k) / (1 - k);
-        m = (1 - g + k) / (1 - k);
-        y = (1 - b + k) / (1 - k);
+        c = 1 - ( r / 255 );
+        m = 1 - ( g / 255 );
+        y = 1 - ( b / 255 );
 
-        return new Color(c*255, m*255, y*255);
+//        c = (1 - r - k) / (1 - k);
+//        m = (1 - g + k) / (1 - k);
+//        y = (1 - b + k) / (1 - k);
+//
+//        c = Math.min(c*255, 255);
+//        m = Math.min(m*255, 255);
+//        y = Math.min(y*255, 255);
+
+        return new Color(c, m, y);
     }
 
     @Override
     public String printResult(Color colorResult){
-        return "C: "+ (int) colorResult.getComponent1() +
-                "\nM: "+ (int) colorResult.getComponent2() +
-                "\nY: " + (int) colorResult.getComponent3();
+        return "C: "+ colorResult.getComponent1() +
+                "\nM: "+ colorResult.getComponent2() +
+                "\nY: " + colorResult.getComponent3();
     }
 }
